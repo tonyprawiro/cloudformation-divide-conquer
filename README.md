20a0cec345afe495fffa91e830d0d96f766a72ec
thadimulyono <thadimulyono@wwf.panda.org>
2015-11-09 21:30:14 +0800

    Added the conquer.py script and updated README file

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cloudformation-divide-conquer
-A collection of simple python scripts to divide and conquer CloudFormation template file into manageable chunks.
+A collection of simple python scripts to divide and conquer a CloudFormation template file into manageable chunks.
+
+## The idea
+
+Writing a CloudFormation means writing a long, monolothic JSON file that quickly gets into a huge mess and difficult to follow.
+
+Amazon has provided CloudFormation Designer tool, however it is still a relatively new tool (released in Oct 2015) and isn't very easy to use.
+
+There are other tools that help you write a better template, such as strotosphere and cfndsl. They are great tools and are really helpful, however most try to be an abstraction tool. This means, you need to learn their abstraction language and such tools need to be updated regularly to keep with the evolution of template format.
+
+cloudformation-divide-conquer doesn't try to be an abstraction tool. It just helps you to break a template into managable chunks, so that it is easier for you to seek and modify information of any section of the template. Then it also help you gather the chunks into one big template file that CloudFormation accepts. It does not try to be opinionated. It just helps you manage your template easier.
+
+## divide.py
+
+Usage:
+
+```bash
+divide.py original-template.json output-splitup.json
+```
+
+## conquer.py
+
+Usage:
+
+```bash
+conquer.py input-splitup.json output-template.json
+```
---
+++ conquer.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+import sys
+import json
+import os.path
+from collections import OrderedDict
+
+sCWD = os.getcwd()
+
+# Make sure input filename is passed to the script
+sInputFile = ""
+try:
+	sInputFile = sys.argv[1]
+except:
+	print "E/ Unable to find input file name. Usage: conquer.py input.json output.json"
+	sys.exit(1)
+
+# Make sure output filename is passed to the script
+sOutputFile = ""
+try:
+	sOutputFile = sys.argv[2]
+except:
+	print "E/ Unable to find output file name. Usage: conquer.py input.json output.json"
+	sys.exit(1)
+
+# Determine the full path name of the input/output json file
+sInputFile = "%s/%s" % (sCWD, sInputFile)
+sOutputFile = "%s/%s" % (sCWD, sOutputFile)
+
+# Make sure input and output file isn't the same
+if sInputFile == sOutputFile:
+	print "E/ Input and output file can't be the same"
+	sys.exit(1)
+
+# Try to open and parse json input file
+stInputJSON = {}
+try:
+	sFileContent = ""
+	with open(sInputFile) as data_file:
+		stInputJSON = json.load(data_file, object_pairs_hook=OrderedDict)
+except:
+	print "E/ Unable to open or parse template file: %s" % (sInputFile)
+	sys.exit(1)
+
+# Make sure it has CloudFormation signature
+try:
+	sAWSTemplateFormatVersion = stInputJSON["AWSTemplateFormatVersion"]
+except:
+	print "E/ Unable to determine CloudFormation template format version. Is this a valid CloudFormation template ?"
+	sys.exit(1)
+
+# It has ? Ok good, let's prepare the output
+stOutput = OrderedDict({
+	"AWSTemplateFormatVersion": sAWSTemplateFormatVersion
+})
+
+# Now expand "Resource" node
+stOutputResources = OrderedDict({})
+stInputResources = OrderedDict({})
+try:
+	stInputResources = stInputJSON["Resources"]
+except:
+	print "N/ Unable to access Resources section. Does the template has any Resource at all ?"
+
+# If there is any Resources, iterate
+for sInputResourceID, value in stInputResources.items():
+	sOutputResourceID = sInputResourceID
+	resultValue = value
+	if isinstance(value, str) or isinstance(value, unicode):
+		aSegments = value.split("::")
+		if len(aSegments) == 3 and aSegments[0] == "Resources":
+			sSegmentResourceType = aSegments[1]
+			sSegmentResourceID = aSegments[2]
+			if sSegmentResourceID == "~":
+				sSegmentResourceID = sInputResourceID
+			sValueFile = "%s/Resources/%s/%s.json" % (sCWD, sSegmentResourceType, sSegmentResourceID)
+			if os.path.isfile(sValueFile):
+				try:
+					with open(sValueFile) as value_file:
+						resultValue = json.load(value_file)
+				except:
+					print "W/ Parsing `%s`: file %s contains invalid JSON object" % (sInputResourceID, sValueFile)
+			else:
+				# The resourcetype/id.json file does not exist
+				print "N/ Parsing `%s`: unable to locate file: %s" % (sInputResourceID, sValueFile)
+		else:
+			# Value isn't in "Resources::yeah::whatever" format 
+			print "N/ Parsing `%s`: value is string but isn't in supported pointer format: %s" % (sInputResourceID, value)
+	else:
+		# Value is not a string
+		print "N/ Parsing `%s`: value isn't a string" % (sInputResourceID)
+
+	stOutputResources[sOutputResourceID] = resultValue
+
+# Hook in the Resources node
+stOutput["Resources"] = stOutputResources
+
+# Write output
+try:
+	f = open(sOutputFile, 'w')
+	f.write(json.dumps(stOutput, indent=2))
+	f.close()
+except Exception, e:
+	print "E/ Unable to write to output file: %s (%s)" % (sOutputFile, str(e))
